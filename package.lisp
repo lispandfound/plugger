019012d5a1d3987aebb692ef11b88ebdcbefc282
Jake Faulkner <jakefaulkn@gmail.com>
2016-04-29 13:17:04 +1200

    Add missing packages

--- package.lisp
+++ package.lisp
@@ -2,9 +2,9 @@
 
 (defpackage #:plugger
   (:use #:cl #:cl-fad)
-  (:export load-plugins reset-plugins defplugfun defplugmac defplugvar))
+  (:export load-plugins reset-plugins defplugfun defplugmac defplugvar *plugger-hooks* *plugin-package* defplughook with-plug-hook trigger-hook))
 
-(defpackage #:plugger-user
+(defpackage #:plugger-plugins
   (:use #:cl #:plugger #:lisp-unit))
 (defpackage #:plugger-test
-  (:use #:cl #:plugger #:plugger-user #:lisp-unit))
+  (:use #:cl #:plugger #:plugger-plugins #:lisp-unit))
