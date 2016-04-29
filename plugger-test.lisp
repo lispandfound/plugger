d5d24e6a5ddebcf351cc1bde87400c528ee77903
Jake Faulkner <jakefaulkn@gmail.com>
2016-04-29 13:18:14 +1200

    Add test cases for hooks

--- plugger-test.lisp
+++ plugger-test.lisp
@@ -26,6 +26,22 @@
     (assert-equal 2 success )
     (assert-equal '(("b" . :success)
                     ("a" . :success)) loaded )))
+(define-test plugin-defhook-test
+  (setf *plugger-hooks* nil)
+  (assert-equal (defplughook :test) '((:test))))
+(define-test plugin-with-hook-test
+  (setf *plugger-hooks* nil)
+  (defplughook :test)
+  (defun hook-test () 0)
+  (with-plug-hook 'test :test #'hook-test)
+  (assert-equal 2 (length (car *plugger-hooks*))))
+(define-test plugin-hook-test
+  (setq *plugger-hooks* nil)
+  (defplughook :test)
+  (with-plug-hook 'test :test (lambda () 0))
+  (multiple-value-bind (success results) (trigger-hook :test ())
+    (assert-equal success 1)
+    (assert-equal results '((test :success (0))))))
 (define-test plugin-import-test
   (reset-plugins)
   (load-plugins "./test_plugins/import-test" :die-on-error t))
