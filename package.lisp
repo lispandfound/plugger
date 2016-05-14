7c38faadaab4073f421ab56ee9a4dd0abaa6054b
Jake Faulkner <jakefaulkn@gmail.com>
2016-05-14 22:42:38 +1200

    Add extra macros missing from symbols list
    Completes the functionality that was missing, but technically available

--- package.lisp
+++ package.lisp
@@ -1,8 +1,7 @@
 ;;;; package.lisp
-
 (defpackage #:plugger
   (:use #:cl #:cl-fad)
-  (:export load-plugins reset-plugins defplugfun defplugmac defplugvar *plugger-hooks* *plugin-package* defplughook with-plug-hook trigger-hook remove-hook functions-for-hook hook-for-function remove-hook-func defapifun defapimac defapivar))
+  (:export load-plugins reset-plugins defplugfun defplugmac defplugvar *plugger-hooks* *api-package* *plugin-package* defplughook with-plug-hook trigger-hook remove-hook functions-for-hook hook-for-function remove-hook-func defapifun defapimac defapivar set-plugin-package set-api-package))
 
 (defpackage #:plugger-plugin-user)
 (defpackage #:plugger-plugins
